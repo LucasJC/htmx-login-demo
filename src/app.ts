1449aaf1fa1184f894907c84d4987347adba1fea
LucasJC <lucas.couchot@gmail.com>
2021-02-10 02:11:27 -0300

    Implement simple log in logic with redirect

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 import express from 'express';
 import path from 'path';
+import bodyParser from 'body-parser';
+
 
 const app = express();
-console.log("Serving files in " + path.join(__dirname, "public"));
-app.use('/', express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.use("/api/login", function (req: express.Request, res: express.Response) {
+  if (req.body.email === "valid@email.com" && req.body.password === "password") {
+    setTimeout( () => {
+      res.status(302);
+      res.setHeader("HX-Redirect", "/home.html");
+      res.send("ok!");
+    }, 2000);
+  } else {
+    setTimeout(() => {
+      const message = "Invalid credentials";
+      res.send(`<div class="notification is-danger" >${message}</div>`);
+    }, 2000);
+  }
+});
+
 app.listen(3000, () => {
   console.log("Up on port 3000!");
 })
